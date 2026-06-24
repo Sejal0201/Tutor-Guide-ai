107db44c4c322a4ebe317dfa85095d949cee9622
Sejal0201 <jainsejal0204@gmail.com>
2026-06-24 19:55:45 +0530

    Added Neon PostgreSQL database

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./tutorguide.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    DATABASE_URL
 )
 
 SessionLocal = sessionmaker(
@@ -15,6 +18,7 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
